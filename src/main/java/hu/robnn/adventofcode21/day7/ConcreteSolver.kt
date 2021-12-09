672231cf6c3664ca03448e54e8e584ebffcce3c6
robnn <robin.kurovszky@gmail.com>
2021-12-09 17:06:22 +0100

    Rework day7

--- src/main/java/hu/robnn/adventofcode21/day7/ConcreteSolver.kt
+++ src/main/java/hu/robnn/adventofcode21/day7/ConcreteSolver.kt
@@ -1,6 +1,7 @@
 package hu.robnn.adventofcode21.day7
 
 import hu.robnn.adventofcode21.Solver
+import kotlin.math.absoluteValue
 
 fun main() {
     val concreteSolver = ConcreteSolver()
@@ -10,13 +11,29 @@ fun main() {
 class ConcreteSolver: Solver {
 
     override fun solvePart1(): String {
-        TODO()
+        return calcLeastExpensiveAlignment().toString()
     }
 
     override fun solvePart2(): String {
-        TODO()
+        return calcLeastExpensiveAlignment(true).toString()
     }
 
     override fun getInputName() = "day7_input1.txt"
 
+    private fun calcLeastExpensiveAlignment(shouldCalcIncrementally: Boolean = false): Int {
+        val originalPositions = readInput().first().split(",").map { it.toInt() }
+        val neededFuelCalculations = mutableListOf<Int>()
+        originalPositions.forEach { x ->
+            val neededFuelOnX = originalPositions.map { y -> x - y }
+                    .map {
+                        if (shouldCalcIncrementally)
+                            (0..it.absoluteValue).reduce { a, b -> a + b }
+                        else
+                            it.absoluteValue
+                    }.reduce { a, b -> a + b }
+            neededFuelCalculations.add(neededFuelOnX)
+        }
+
+        return neededFuelCalculations.minOf { it }
+    }
 }
