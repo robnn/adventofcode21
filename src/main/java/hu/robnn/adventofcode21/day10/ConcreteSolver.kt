1ed7126186c32a8bc04d82b761a162cfbdf5a1d6
robnn <robin.kurovszky@gmail.com>
2021-12-14 19:48:00 +0100

    Day9 and 10

--- src/main/java/hu/robnn/adventofcode21/day10/ConcreteSolver.kt
+++ src/main/java/hu/robnn/adventofcode21/day10/ConcreteSolver.kt
@@ -1,6 +1,7 @@
 package hu.robnn.adventofcode21.day10
 
 import hu.robnn.adventofcode21.Solver
+import java.util.*
 
 fun main() {
     val concreteSolver = ConcreteSolver()
@@ -9,14 +10,80 @@ fun main() {
 
 class ConcreteSolver: Solver {
 
+    private val openToCloseTokens = mapOf(
+            "{" to "}",
+            "<" to ">",
+            "(" to ")",
+            "[" to "]"
+    )
+
     override fun solvePart1(): String {
-        TODO()
+        val score = mapOf(
+                ")" to 3L,
+                "]" to 57L,
+                "}" to 1197L,
+                ">" to 25137L
+        )
+        val input = readInput().map { it.chunked(1) }
+        val lineToIllegalChar = getLinesToIllegalChars(input)
+        return lineToIllegalChar.values.map { score[it]!! }.reduce { a, b -> a + b}.toString()
     }
 
     override fun solvePart2(): String {
-        TODO()
+        val score = mapOf(
+                ")" to 1L,
+                "]" to 2L,
+                "}" to 3L,
+                ">" to 4L
+        )
+        val input = readInput()
+        val lineToIllegalChar = getLinesToIllegalChars(input.map { it.chunked(1) })
+        val onlyIncompleteLines = input.minus(lineToIllegalChar.keys).map { it.chunked(1) }
+        val lineToAutocompleteScore = mutableMapOf<String, Long>()
+        onlyIncompleteLines.forEach { row ->
+            val stack = Stack<String>()
+            val toBeComplete = mutableListOf<String>()
+            row.forEach inside@{
+                if (openToCloseTokens.keys.contains(it)) {
+                    stack.push(it)
+                } else {
+                    stack.pop()
+                }
+            }
+            while (!stack.empty()) {
+                toBeComplete.add(openToCloseTokens[stack.pop()]!!)
+            }
+
+            var totalScore = 0L
+            toBeComplete.forEach {
+                totalScore *= 5
+                totalScore += score[it]!!
+            }
+            lineToAutocompleteScore[row.joinToString("")] = totalScore
+        }
+
+        return lineToAutocompleteScore.values.sorted()[(lineToAutocompleteScore.values.size) / 2].toString()
     }
 
     override fun getInputName() = "day10_input1.txt"
 
+    private fun getLinesToIllegalChars(
+            input: List<List<String>>): MutableMap<String, String> {
+        val lineToIllegalChar = mutableMapOf<String, String>()
+        input.forEach { row ->
+            val stack = Stack<String>()
+            row.forEach inside@{
+                if (openToCloseTokens.keys.contains(it)) {
+                    stack.push(it)
+                } else {
+                    val popped = stack.pop()
+                    if (it != openToCloseTokens[popped]) {
+                        lineToIllegalChar[row.joinToString("")] = it
+                        return@inside
+                    }
+                }
+            }
+        }
+        return lineToIllegalChar
+    }
 }
--- src/main/java/hu/robnn/adventofcode21/day9/ConcreteSolver.kt
+++ src/main/java/hu/robnn/adventofcode21/day9/ConcreteSolver.kt
@@ -12,61 +12,79 @@ class ConcreteSolver: Solver {
     override fun solvePart1(): String {
         val input = readInput()
         val matrix = input.map { it.chunked(1).map { it.toInt() } }
-        val lowPoints = getLowPointsWithBasins(matrix)
+        val lowPoints = getLowPoints(matrix)
 
-        return lowPoints.map { it.first + 1 }.sum().toString()
+        return lowPoints.map { it + 1 }.sum().toString()
     }
 
-
+    /**
+     * Mainly copied mbali's solution https://github.com/mbali/advent-of-code-2021/blob/main/src/Day09.kt
+     */
     override fun solvePart2(): String {
         val input = readInput()
         val matrix = input.map { it.chunked(1).map { it.toInt() } }
-        val lowPoints = getLowPointsWithBasins(matrix)
-
-        return lowPoints.map { it.second }.sorted().reversed().chunked(3)[0].reduce { a, b -> a * b }.toString()
+        val basinMap = MutableList(matrix.size) { row ->
+            MutableList(matrix[row].size) { -1 }
+        }
+        var basinIndex = 0
+        do {
+            val seed = matrix.indices.firstNotNullOfOrNull { row ->
+                matrix[row].indices.firstNotNullOfOrNull { col ->
+                    if (matrix[row][col] < 9 && basinMap[row][col] < 0) row to col
+                    else null
+                }
+            }
+            seed?.let { calcBasinSize(it.first, it.second, matrix, basinMap, basinIndex++) }
+        } while (seed != null)
+        return basinMap.flatMap { it.toList() }
+                .filter { it > 0 }
+                .groupingBy { it }
+                .eachCount().values
+                .sortedDescending()
+                .take(3).reduce { a, b -> a * b }.toString()
     }
 
     override fun getInputName() = "day9_input1.txt"
 
-    private fun getLowPointsWithBasins(
-            matrix: List<List<Int>>): MutableList<Pair<Int, Int>> {
-        val lowPoints = mutableListOf<Pair<Int, Int>>()
+    private fun getLowPoints(
+            matrix: List<List<Int>>): MutableList<Int> {
+        val lowPoints = mutableListOf<Int>()
         matrix.forEachIndexed { index1, row ->
             row.forEachIndexed { index2, num ->
                 if (index1 == 0) {
                     if (index2 == 0) {
                         //left upper corner
                         if (num < matrix[index1][index2 + 1] && num < matrix[index1 + 1][index2]) {
-                            lowPoints.add(num to calcBasinSize(true, false, true, false, index1, index2, matrix, index1, index2))
+                            lowPoints.add(num)
                         }
                     } else if (index2 == row.size - 1) {
                         //right upper corner
                         if (num < matrix[index1][index2 - 1] && num < matrix[index1 + 1][index2]) {
-                            lowPoints.add(num to calcBasinSize(true, false, false, true, index1, index2, matrix, index1, index2))
+                            lowPoints.add(num)
                         }
                     } else {
                         //first row
                         if (num < matrix[index1][index2 - 1] && num < matrix[index1][index2 + 1] && num <
                                 matrix[index1 + 1][index2]) {
-                            lowPoints.add(num to calcBasinSize(true, false, true, true, index1, index2, matrix, index1, index2))
+                            lowPoints.add(num)
                         }
                     }
                 } else if (index1 == matrix.size - 1) {
                     if (index2 == 0) {
                         //left lower corner
                         if (num < matrix[index1][index2 + 1] && num < matrix[index1][index2 - 1]) {
-                            lowPoints.add(num to calcBasinSize(false, true, true, false, index1, index2, matrix, index1, index2))
+                            lowPoints.add(num)
                         }
                     } else if (index2 == row.size - 1) {
                         //right lower corner
                         if (num < matrix[index1][index2 - 1] && num < matrix[index1][index2 - 1]) {
-                            lowPoints.add(num to calcBasinSize(false, true, false, true, index1, index2, matrix, index1, index2))
+                            lowPoints.add(num)
                         }
                     } else {
                         //last row
                         if (num < matrix[index1][index2 - 1] && num < matrix[index1][index2 + 1] && num <
                                 matrix[index1 - 1][index2]) {
-                            lowPoints.add(num to calcBasinSize(false, true, true, true, index1, index2, matrix, index1, index2))
+                            lowPoints.add(num)
                         }
                     }
                 } else {
@@ -74,19 +92,19 @@ class ConcreteSolver: Solver {
                         //left edge
                         if (num < matrix[index1][index2 + 1] && num < matrix[index1 - 1][index2]
                                 && num < matrix[index1 + 1][index2]) {
-                            lowPoints.add(num to calcBasinSize(true, true, true, false, index1, index2, matrix, index1, index2))
+                            lowPoints.add(num)
                         }
                     } else if (index2 == row.size - 1) {
                         //right edge
                         if (num < matrix[index1][index2 - 1] && num < matrix[index1 + 1][index2]
                                 && num < matrix[index1 - 1][index2]) {
-                            lowPoints.add(num to calcBasinSize(true, true, false, true, index1, index2, matrix, index1, index2))
+                            lowPoints.add(num)
                         }
                     } else {
                         //last row
                         if (num < matrix[index1][index2 - 1] && num < matrix[index1][index2 + 1] &&
                                 num < matrix[index1 - 1][index2] && num < matrix[index1 + 1][index2]) {
-                            lowPoints.add(num to calcBasinSize(true, true, true, true, index1, index2,  matrix, index1, index2))
+                            lowPoints.add(num)
                         }
                     }
                 }
@@ -95,51 +113,22 @@ class ConcreteSolver: Solver {
         return lowPoints
     }
 
-    private fun calcBasinSize(index1RaiseAllowed: Boolean, index1DecreaseAllowed: Boolean, index2RaiseAllowed: Boolean,
-                              index2DecreaseAllowed: Boolean, index1: Int, index2: Int,
-                              matrix: List<List<Int>>,
-                              startIndex1: Int, startIndex2: Int,
-                              prevIndex1: Int = -1, prevIndex2: Int = -1,
-    startIndexCount: Int = 0): Int {
-        var size = 0
+    private val neigboursDirections = listOf(
+            0 to 1,
+            0 to -1,
+            1 to 0,
+            -1 to 0
+    )
 
-        val startIndexCount2 =  if (startIndex1 == index1 && startIndex2 == index2) {
-            startIndexCount + 1
-        } else {
-            startIndexCount
-        }
-        if (startIndexCount2 == 2) {
-            return size
-        }
-        println("current num of mat [$index1][$index2]: ${matrix[index1][index2]}")
-        if (matrix[index1][index2] == 9) {
-           return size
-        } else {
-            size++
-        }
-
-        if (index1RaiseAllowed) {
-            size += calcBasinSize(matrix.size > index1 + 2, !(prevIndex1 == index1 - 1 && prevIndex2 == index2),
-                    index2RaiseAllowed,
-                    index2DecreaseAllowed, index1 + 1, index2, matrix, startIndex1, startIndex2, index1, index2, startIndexCount2)
-        }
-        if (index1DecreaseAllowed) {
-            size += calcBasinSize(!(prevIndex1 == index1 + 1 && prevIndex2 == index2), 0 <= index1 - 2,
-                    index2RaiseAllowed,
-                    index2DecreaseAllowed, index1 - 1, index2, matrix, startIndex1, startIndex2, index1, index2, startIndexCount2)
-        }
-        if (index2RaiseAllowed) {
-            size += calcBasinSize(index1RaiseAllowed, index1DecreaseAllowed, matrix[index1].size > index2 + 2,
-                    !(prevIndex1 == index1 && prevIndex2 == index2 - 1), index1, index2 + 1, matrix, startIndex1,
-                    startIndex2, index1, index2, startIndexCount2)
-        }
-        if (index2DecreaseAllowed) {
-            size += calcBasinSize(index1RaiseAllowed, index1DecreaseAllowed, !(prevIndex1 == index1 && prevIndex2 ==
-                    index2 + 1),
-                    0 <= index2 - 2, index1, index2 - 1, matrix, startIndex1, startIndex2, index1, index2, startIndexCount2)
-        }
-        println("basinSizeCurrently $size")
-        return size
+    private fun calcBasinSize(index1: Int, index2: Int, matrix: List<List<Int>>, basinMarks:
+    MutableList<MutableList<Int>>, basinIndex: Int) {
+        if (matrix[index1][index2] == 9 || basinMarks[index1][index2] >= 0) return
+        basinMarks[index1][index2] = basinIndex
+        neigboursDirections.map { it.first + index1 to it.second + index2 }
+                .filter { it.first in matrix.indices && it.second in matrix[index1].indices }
+                .forEach {
+                    calcBasinSize(it.first, it.second, matrix, basinMarks, basinIndex)
+                }
     }
 
 }
